630c1dba118a3bbc15656584063cb7f3e13de1d5
clembapt <73126302+clembapt@users.noreply.github.com>
2021-06-12 20:10:22 +0000

    IT FUCKING WORKS

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 import discord
 import os
-from discord.ext import commands, tasks
+from discord.ext import  tasks
 
 client = discord.Client()
-bot = commands.Bot("!")
+
 target_channel_id = 853227884055232515
 
 @client.event
@@ -15,23 +15,28 @@ async def on_message(message):
     if message.author == client.user:
         return
 
-    if message.content.startswith('!xqcow'):
+    if message.content.startswith('xqcow'):
         await message.channel.send('https://www.twitch.tv/xqcow')
+    
+
 
 @tasks.loop(hours=24)
 async def called_once_a_day():
-    message_channel = bot.get_channel(target_channel_id)
+    message_channel = client.get_channel(target_channel_id)
     await message_channel.send("sa fais 24h")
 
 @called_once_a_day.before_loop
 async def before():
-    await bot.wait_until_ready()
+    await client.wait_until_ready()
     print("Finished waiting")
+
+
+
 
 called_once_a_day.start()    
  
 
 
 token = os.environ['token'] 
-bot.run(token)
+
 client.run(token)
